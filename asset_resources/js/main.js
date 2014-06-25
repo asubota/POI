b77c4b50931dc12632a56cb9f14076510f915a6a
Aleksandr <subotaav@gmail.com>
2014-06-25 22:01:41 +0300

    Animated buttons

--- asset_resources/js/main.js
+++ asset_resources/js/main.js
@@ -2,9 +2,9 @@ var map = L.map('map').setView([51, 31], 10);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
 PoiManager.addRegions({
-  modalRegion: '.poi-modal-region',
-  poisRegion:  '.poi-list-region',
-  detailsRegion:  '.poi-details-region',
+  modalRegion   :  '.poi-modal-region',
+  poisRegion    :  '.poi-list-region',
+  detailsRegion :  '.poi-details-region',
 });
 
 PoiManager.on('start', function(){
