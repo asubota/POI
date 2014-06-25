f83e4bb0686b3195dd715bcb769d33f039b79e9e
asubota <asubota@cogniance.com>
2014-06-25 14:38:29 +0300

    marionette

--- asset_resources/js/main.js
+++ asset_resources/js/main.js
@@ -1,5 +1,20 @@
-new AppView();
-
 var map = L.map('map').setView([51, 31], 10);
-
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+PoiManager.addRegions({
+  modalRegion: '.poi-modal-region',
+  poisRegion:  '.poi-list-region',
+});
+
+PoiManager.on('start', function(){
+  var pois = new PoiManager.PoiCollection();
+  pois.fetch();
+
+  var poisView = new PoiManager.PoisView({
+    collection: pois
+  });
+
+  PoiManager.poisRegion.show(poisView);
+});
+
+PoiManager.start();
