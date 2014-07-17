97e6d33e03779fe655207480cd06f8c02566f559
asubota <asubota@cogniance.com>
2014-07-17 15:45:54 +0300

    Rspec ready

--- spec/REST/poi_spec.rb
+++ spec/REST/poi_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
-describe 'POI' do
+describe 'CRUD' do
+  describe 'create' do
+  end
 
+  describe 'read' do
+    context 'all' do
+      before(:all) { get '/api/pois' }
+
+      it { expect(last_response.status).to eq 200 }
+      it { expect(last_response.header['Content-Type']).to include 'application/json' }
+    end
+
+    context 'single' do
+
+    end
+  end
+
+  describe 'update' do
+
+  end
+
+  describe 'delete' do
+
+  end
 end
